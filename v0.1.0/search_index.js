64af20030e1962df0fddfaaa4b52a9b2b36b3afd
autodocs <autodocs>
2018-12-07 20:22:46 +0000

    build based on a65dcd8

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,59 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Documentation",
+    "title": "Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#SplittingOperators.@Lie",
+    "page": "Documentation",
+    "title": "SplittingOperators.@Lie",
+    "category": "macro",
+    "text": "@Lie( push_t, push_v )\n\nApply the first order Lie splitting\n\npush_t and push_v are two function calls with\n`dt` as argument.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#SplittingOperators.@Strang",
+    "page": "Documentation",
+    "title": "SplittingOperators.@Strang",
+    "category": "macro",
+    "text": "@Strang( push_t, push_v )\n\nApply the second order Strang splitting\n\npush_t and push_v are two function calls with\n`dt` as argument.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#SplittingOperators.@TripleJump",
+    "page": "Documentation",
+    "title": "SplittingOperators.@TripleJump",
+    "category": "macro",
+    "text": "@TripleJump( push_t, push_v )\n\nApply the fourth order Triple Jump splitting\n\npush_t and push_v are two function calls with\n`dt` as argument.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#SplittingOperators.@Order6",
+    "page": "Documentation",
+    "title": "SplittingOperators.@Order6",
+    "category": "macro",
+    "text": "@Order6( push_t, push_v )\n\nApply the sixth order splitting\n\npush_t and push_v are two function calls with\n`dt` as argument.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#SplittingOperators.@Magic",
+    "page": "Documentation",
+    "title": "SplittingOperators.@Magic",
+    "category": "macro",
+    "text": "@Magic( push_t, push_v )\n\nApply the second order modified Strang splitting\n\nwith tan(dt/2) instead of dt/2 for push_t and sin(dt)\ninstead of dt for push_v\n\npush_t and push_v are two function calls with\n`dt` as argument.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#SplittingOperators.jl-Documentation-1",
+    "page": "Documentation",
+    "title": "SplittingOperators.jl Documentation",
+    "category": "section",
+    "text": "@Lie\n@Strang\n@TripleJump\n@Order6\n@Magic"
+},
+
+]}
